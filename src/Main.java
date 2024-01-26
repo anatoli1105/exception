d45607f76ea392fe6d67cf8f26e86c6e2588a8f4
anatolii <anatoliise@mail.ru>
2024-01-27 01:05:54 +0300

    exception

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,37 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static final String REGEX ="^[a-zA0-9_]*$" ;
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-
-
+        try {
+            metodForData("jj","jj","jj");
+            System.out.println("ok.");
+        }catch (WrongLoginException|WrongPasswordException o){
+            System.out.println(o.getMessage());
         }
     }
+    public static void metodForData(String login, String password, String confirmPassword) {
+        if (login.length() > 20) {
+            throw new WrongLoginException("!длина больше 20!");
+        }
+        if(!login.matches(REGEX)){
+            throw new WrongLoginException("содержит неправильные символы");
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("password и confirmPassword не равны");
+        }
+        if(!password.matches(REGEX)){
+            throw new WrongPasswordException("содержит неправильные символы");
+        }
+
+
+    }
+
+
+
+
 }
